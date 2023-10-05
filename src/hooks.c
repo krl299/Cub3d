d4279ebdffde55be1b67b998e7968300d838bd44
mandriic <mandriic@student.42malaga.com>
2023-10-05 13:02:03 +0200

    creacion funciones 2xy, 2yx, 1/2x 1/2y

--- src/hooks.c
+++ src/hooks.c
@@ -19,159 +19,100 @@ void debug(t_vars *vars)
 	// // write(1, ft_itoa(vars->mini_unit_xy[1]), ft_strlen(ft_itoa(vars->mini_unit_xy[1])));
 	// write(1, "\n", 1);
 }
-
+void ft_move_x(t_vars *vars, int corrector_x)
+{
+	vars->map_vars->cont_y += 1 * corrector_x;
+	vars->mini_unit->instances[0].x += SPEEX;
+}
+void ft_move_y(t_vars *vars, int corrector_y)
+{
+	vars->map_vars->cont_x += 1 * corrector_y;
+	vars->mini_unit->instances[0].y += SPEEX;
+}
 void ft_hook(void* param)
 {
 	t_vars *vars;
+
+	static int fx = 0;
+	static int fy = 0;
+	int corector_x = 1;
+	int corector_y = 1;
+	float sect_calc;
 	vars = param;
 			if (vars->map_vars->mini_u_angle >= M_PI * 2)
 		vars->map_vars->mini_u_angle = 0.0000000001;
 			if (vars->map_vars->mini_u_angle < 0.0000000001)
 		vars->map_vars->mini_u_angle =  M_PI * 2;
-
+	if (vars->map_vars->go_angle >= 9 && vars->map_vars->go_angle <= 15)
+		corector_y = -1;
+	if (vars->map_vars->go_angle >= 17 && vars->map_vars->go_angle <= 23)
+	{
+		corector_x = -1;
+		corector_y = -1;
+	}
+	if (vars->map_vars->go_angle >= 25 && vars->map_vars->go_angle <= 31)
+		corector_x = -1;
+	// if (mlx_is_key_down(vars->mlx, MLX_KEY_UP))
+	sect_calc = vars->map_vars->go_angle;
 
 	if (mlx_is_key_down(vars->mlx, MLX_KEY_ESCAPE))
 		mlx_close_window(vars->mlx);
 
 
 	else if (mlx_is_key_down(vars->mlx, MLX_KEY_UP))
-	{	
-		// printf("instances y = %d", vars->mini_unit->instances[0].y/vars->map_vars->len_char);
-		// if(vars->map_vars->map[(vars->map_vars->cont_x - 1)/ vars->map_vars->len_char][(vars->map_vars->cont_y)/ vars->map_vars->len_char] != '1' &&\
-		// 	vars->map_vars->map[(vars->map_vars->cont_x - 1) / vars->map_vars->len_char][(vars->map_vars->cont_y + vars->map_vars->len_char - 1) / vars->map_vars->len_char] != '1')
-		// {
-		if(vars->map_vars->go_angle > 24 && vars->map_vars->go_angle <= 32)
+	{
+		if (vars->map_vars->go_angle >= 31 || vars->map_vars->go_angle <= 1)
+			ft_move_y(vars, corector_y);
+		if (vars->map_vars->go_angle > 1 && vars->map_vars->go_angle <= 3)
 		{
-			vars->map_vars->cont_x++;
-			vars->mini_unit->instances[0].y += SPEEX;
-			vars->map_vars->cont_y--;
-			vars->mini_unit->instances[0].x -= SPEEX;
-			if(vars->map_vars->go_angle > 24 && vars->map_vars->go_angle <= 28)
+			if (fy == 2) //crear funciones 2yx, 2xy, xy, 1/2x 1/2y
 			{
-				vars->map_vars->cont_y--;
-				vars->mini_unit->instances[0].x -= SPEEX;
+				ft_move_x(vars, corector_x);
+				fy = 0;
 			}
 			else
 			{
-				vars->map_vars->cont_x++;
-				vars->mini_unit->instances[0].y += SPEEX;
+				fy += 1;
+				ft_move_y(vars, corector_y);
 			}
 		}
-		else if(vars->map_vars->go_angle > 16  && vars->map_vars->go_angle <= 24)
+		if (vars->map_vars->go_angle > 3 && vars->map_vars->go_angle <= 5)
 		{
-			vars->map_vars->cont_x--;
-			vars->mini_unit->instances[0].y -= SPEEX;
-			vars->map_vars->cont_y--;
-			vars->mini_unit->instances[0].x -= SPEEX;
-			if(vars->map_vars->go_angle > 16 && vars->map_vars->go_angle <= 20)
+			if (fy == 1)//crear funciones 2yx, 2xy, xy, 1/2x 1/2y
 			{
-				vars->map_vars->cont_x--;
-				vars->mini_unit->instances[0].y -= SPEEX;
+				ft_move_x(vars, corector_x);
+				fy = 0;
 			}
 			else
 			{
-				vars->map_vars->cont_y--;
-				vars->mini_unit->instances[0].x -= SPEEX;
+				fy += 1;
+				ft_move_y(vars, corector_y);
 			}
 		}
-		else if(vars->map_vars->go_angle > 8  && vars->map_vars->go_angle <= 16)
+		if (vars->map_vars->go_angle > 5 && vars->map_vars->go_angle <= 7)
 		{
-			vars->map_vars->cont_x--;
-			vars->mini_unit->instances[0].y -= SPEEX;
-			vars->map_vars->cont_y++;
-			vars->mini_unit->instances[0].x += SPEEX;
-			if(vars->map_vars->go_angle > 12 && vars->map_vars->go_angle <= 16)
+			if (fx == 2)//crear funciones 2yx, 2xy, xy, 1/2x 1/2y
 			{
-				vars->map_vars->cont_x--;
-				vars->mini_unit->instances[0].y -= SPEEX;
+				ft_move_y(vars, corector_y);
+				fx = 0;
 			}
 			else
 			{
-				vars->map_vars->cont_x++;
-				vars->mini_unit->instances[0].x += SPEEX;
+				fx += 1;
+				ft_move_x(vars, corector_x);
 			}
 		}
-		// if (vars->map_vars->go_angle >= 25 || vars->map_vars->go_angle < 8)
-		// {
-		// 	vars->map_vars->cont_x++;
-		// 	vars->mini_unit->instances[0].y += SPEEX;
-		// }
-		// if (vars->map_vars->go_angle > 0 && vars->map_vars->go_angle < 3)
-		// {
-		// 	vars->map_vars->cont_x++;
-		// 	vars->mini_unit->instances[0].y += SPEEX;
-		// }
-		// if (vars->map_vars->go_angle >= 3 && vars->map_vars->go_angle < 5)
-		// {
-		// 	vars->map_vars->cont_x++;
-		// 	vars->mini_unit->instances[0].y += SPEEX;
-		// 	vars->map_vars->cont_y--;
-		// 	vars->mini_unit->instances[0].x -= SPEEX;
-		// }
-
-		// if (vars->map_vars->go_angle > 16 && vars->map_vars->go_angle <= 32)
-		// {
-		// 	vars->map_vars->cont_y--;
-		// 	vars->mini_unit->instances[0].x -= SPEEX;
-
-		// }
-		// if (vars->map_vars->go_angle >= 8 && vars->map_vars->go_angle < 25)
-		// {
-		// 	vars->map_vars->cont_x--;
-		// 	vars->mini_unit->instances[0].y -= SPEEX;
-		// }
-		// if (vars->map_vars->go_angle > 0 && vars->map_vars->go_angle <= 16)
-		// {
-		// 	vars->map_vars->cont_y++;
-		// 	vars->mini_unit->instances[0].x += SPEEX;
-		// }	
+		if (vars->map_vars->go_angle > 7 && vars->map_vars->go_angle <= 9)
+			ft_move_x(vars, corector_x);
 		if (mlx_is_key_down(vars->mlx, MLX_KEY_LEFT))
 			vars->map_vars->mini_u_angle += 0.04;
 		if (mlx_is_key_down(vars->mlx, MLX_KEY_RIGHT))
 			vars->map_vars->mini_u_angle -= 0.04;
-
 		debug(vars);
-		ft_trace_line(vars);
+		ft_trace_line(vars);	
 	}
 
-	// else if (mlx_is_key_down(vars->mlx, MLX_KEY_DOWN))
-	// {
-		// printf("instances y = %d", vars->mini_unit->instances[0].y/vars->map_vars->len_char);
-		// if((vars->map_vars->map[(vars->map_vars->cont_x + vars->map_vars->len_char) / vars->map_vars->len_char][(vars->map_vars->cont_y + vars->map_vars->len_char - 1) / vars->map_vars->len_char] != '1' &&\
-		// 	vars->map_vars->map[(vars->map_vars->cont_x+ vars->map_vars->len_char)/ vars->map_vars->len_char][vars->map_vars->cont_y  / vars->map_vars->len_char] != '1') || \
-		// 	(vars->map_vars->map[(vars->map_vars->cont_x - 1)/ vars->map_vars->len_char][(vars->map_vars->cont_y)/ vars->map_vars->len_char] != '1' &&\
-		// 	vars->map_vars->map[(vars->map_vars->cont_x - 1) / vars->map_vars->len_char][(vars->map_vars->cont_y + vars->map_vars->len_char - 1) / vars->map_vars->len_char] != '1'))
-		// {
-	// 		if (vars->map_vars->go_angle > 25 || vars->map_vars->go_angle < 16)
-	// 		{
-	// 			vars->map_vars->cont_x--;
-	// 			vars->mini_unit->instances[0].y -= SPEEX;
-	// 			if (vars->map_vars->go_angle > 25)
-	// 			{
-	// 				vars->map_vars->cont_y++;
-	// 				vars->mini_unit->instances[0].x += SPEEX;
-	// 			}
-	// 			else
-	// 			{
-	// 				vars->map_vars->cont_y--;
-	// 				vars->mini_unit->instances[0].x -= SPEEX;
-	// 			}
-	// 		}
-	// 		else 
-	// 		{
-	// 			vars->map_vars->cont_x++;
-	// 			vars->mini_unit->instances[0].y += SPEEX;
-	// 		}
-	// 	// }
-	// 	if (mlx_is_key_down(vars->mlx, MLX_KEY_LEFT))
-	// 		vars->map_vars->mini_u_angle += 0.04;
-	// 	if (mlx_is_key_down(vars->mlx, MLX_KEY_RIGHT))
-	// 		vars->map_vars->mini_u_angle -= 0.04;
-
-	// 	ft_trace_line(vars);
-	// 	debug(vars);
-	// }
-	else if (mlx_is_key_down(vars->mlx, MLX_KEY_LEFT))
+	if (mlx_is_key_down(vars->mlx, MLX_KEY_LEFT))
 	{
 		vars->map_vars->mini_u_angle += 0.04;
 		ft_trace_line(vars);
@@ -183,6 +124,7 @@ void ft_hook(void* param)
 		ft_trace_line(vars);
 
 	}
+}
 
 
 
@@ -248,4 +190,136 @@ void ft_hook(void* param)
 	// 	ft_trace_line(vars);
 	// 	debug(vars);
 	// }
-}
+
+
+
+
+
+	// // printf("instances y = %d", vars->mini_unit->instances[0].y/vars->map_vars->len_char);
+		// // if(vars->map_vars->map[(vars->map_vars->cont_x - 1)/ vars->map_vars->len_char][(vars->map_vars->cont_y)/ vars->map_vars->len_char] != '1' &&\
+		// // 	vars->map_vars->map[(vars->map_vars->cont_x - 1) / vars->map_vars->len_char][(vars->map_vars->cont_y + vars->map_vars->len_char - 1) / vars->map_vars->len_char] != '1')
+		// // {
+		// if(vars->map_vars->go_angle > 24 && vars->map_vars->go_angle <= 32)
+		// {
+		// 	vars->map_vars->cont_x++;
+		// 	vars->mini_unit->instances[0].y += SPEEX;
+		// 	vars->map_vars->cont_y--;
+		// 	vars->mini_unit->instances[0].x -= SPEEX;
+		// 	if(vars->map_vars->go_angle > 24 && vars->map_vars->go_angle <= 28)
+		// 	{
+		// 		vars->map_vars->cont_y--;
+		// 		vars->mini_unit->instances[0].x -= SPEEX;
+		// 	}
+		// 	else
+		// 	{
+		// 		vars->map_vars->cont_x++;
+		// 		vars->mini_unit->instances[0].y += SPEEX;
+		// 	}
+		// }
+		// else if(vars->map_vars->go_angle > 16  && vars->map_vars->go_angle <= 24)
+		// {
+		// 	vars->map_vars->cont_x--;
+		// 	vars->mini_unit->instances[0].y -= SPEEX;
+		// 	vars->map_vars->cont_y--;
+		// 	vars->mini_unit->instances[0].x -= SPEEX;
+		// 	if(vars->map_vars->go_angle > 16 && vars->map_vars->go_angle <= 20)
+		// 	{
+		// 		vars->map_vars->cont_x--;
+		// 		vars->mini_unit->instances[0].y -= SPEEX;
+		// 	}
+		// 	else
+		// 	{
+		// 		vars->map_vars->cont_y--;
+		// 		vars->mini_unit->instances[0].x -= SPEEX;
+		// 	}
+		// }
+		// else if(vars->map_vars->go_angle > 8  && vars->map_vars->go_angle <= 16)
+		// {
+		// 	vars->map_vars->cont_x--;
+		// 	vars->mini_unit->instances[0].y -= SPEEX;
+		// 	vars->map_vars->cont_y++;
+		// 	vars->mini_unit->instances[0].x += SPEEX;
+		// 	if(vars->map_vars->go_angle > 12 && vars->map_vars->go_angle <= 16)
+		// 	{
+		// 		vars->map_vars->cont_x--;
+		// 		vars->mini_unit->instances[0].y -= SPEEX;
+		// 	}
+		// 	else
+		// 	{
+		// 		vars->map_vars->cont_x++;
+		// 		vars->mini_unit->instances[0].x += SPEEX;
+		// 	}
+		// }
+		// if (vars->map_vars->go_angle >= 25 || vars->map_vars->go_angle < 8)
+		// {
+		// 	vars->map_vars->cont_x++;
+		// 	vars->mini_unit->instances[0].y += SPEEX;
+		// }
+		// if (vars->map_vars->go_angle > 0 && vars->map_vars->go_angle < 3)
+		// {
+		// 	vars->map_vars->cont_x++;
+		// 	vars->mini_unit->instances[0].y += SPEEX;
+		// }
+		// if (vars->map_vars->go_angle >= 3 && vars->map_vars->go_angle < 5)
+		// {
+		// 	vars->map_vars->cont_x++;
+		// 	vars->mini_unit->instances[0].y += SPEEX;
+		// 	vars->map_vars->cont_y--;
+		// 	vars->mini_unit->instances[0].x -= SPEEX;
+		// }
+
+		// if (vars->map_vars->go_angle > 16 && vars->map_vars->go_angle <= 32)
+		// {
+		// 	vars->map_vars->cont_y--;
+		// 	vars->mini_unit->instances[0].x -= SPEEX;
+
+		// }
+		// if (vars->map_vars->go_angle >= 8 && vars->map_vars->go_angle < 25)
+		// {
+		// 	vars->map_vars->cont_x--;
+		// 	vars->mini_unit->instances[0].y -= SPEEX;
+		// }
+		// if (vars->map_vars->go_angle > 0 && vars->map_vars->go_angle <= 16)
+		// {
+		// 	vars->map_vars->cont_y++;
+		// 	vars->mini_unit->instances[0].x += SPEEX;
+		// }	
+
+
+	// else if (mlx_is_key_down(vars->mlx, MLX_KEY_DOWN))
+	// {
+		// printf("instances y = %d", vars->mini_unit->instances[0].y/vars->map_vars->len_char);
+		// if((vars->map_vars->map[(vars->map_vars->cont_x + vars->map_vars->len_char) / vars->map_vars->len_char][(vars->map_vars->cont_y + vars->map_vars->len_char - 1) / vars->map_vars->len_char] != '1' &&\
+		// 	vars->map_vars->map[(vars->map_vars->cont_x+ vars->map_vars->len_char)/ vars->map_vars->len_char][vars->map_vars->cont_y  / vars->map_vars->len_char] != '1') || \
+		// 	(vars->map_vars->map[(vars->map_vars->cont_x - 1)/ vars->map_vars->len_char][(vars->map_vars->cont_y)/ vars->map_vars->len_char] != '1' &&\
+		// 	vars->map_vars->map[(vars->map_vars->cont_x - 1) / vars->map_vars->len_char][(vars->map_vars->cont_y + vars->map_vars->len_char - 1) / vars->map_vars->len_char] != '1'))
+		// {
+	// 		if (vars->map_vars->go_angle > 25 || vars->map_vars->go_angle < 16)
+	// 		{
+	// 			vars->map_vars->cont_x--;
+	// 			vars->mini_unit->instances[0].y -= SPEEX;
+	// 			if (vars->map_vars->go_angle > 25)
+	// 			{
+	// 				vars->map_vars->cont_y++;
+	// 				vars->mini_unit->instances[0].x += SPEEX;
+	// 			}
+	// 			else
+	// 			{
+	// 				vars->map_vars->cont_y--;
+	// 				vars->mini_unit->instances[0].x -= SPEEX;
+	// 			}
+	// 		}
+	// 		else 
+	// 		{
+	// 			vars->map_vars->cont_x++;
+	// 			vars->mini_unit->instances[0].y += SPEEX;
+	// 		}
+	// 	// }
+	// 	if (mlx_is_key_down(vars->mlx, MLX_KEY_LEFT))
+	// 		vars->map_vars->mini_u_angle += 0.04;
+	// 	if (mlx_is_key_down(vars->mlx, MLX_KEY_RIGHT))
+	// 		vars->map_vars->mini_u_angle -= 0.04;
+
+	// 	ft_trace_line(vars);
+	// 	debug(vars);
+	// }
